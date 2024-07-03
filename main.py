0b247220d8a7e770c1480ebe5bda5134836dd8cf
saiboT-Damaske <saibot01141@gmail.com>
2024-07-03 17:00:02 +0200

    completed project

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-from flask import Flask, render_template
+import wtforms.validators
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap4
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+class LoginForm(FlaskForm):
+    email = StringField(label='Email',
+                        validators=[DataRequired(), Email(message="Please insert a valid email address")])
+    password = PasswordField(label="Password",
+                             validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap4(app)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        print("yes")
+        if (login_form.email.data == "admin@email.com") and (login_form.password.data == "12345678"):
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template("/login.html", form=login_form)
 
 
 if __name__ == '__main__':
