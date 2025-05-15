097887ed6fe2d4fe3412b43b6616861ca777e881
saiboT-Damaske <saibot01141@gmail.com>
2025-05-15 15:26:48 +0200

    done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap4
 
 
 class LoginForm(FlaskForm):
@@ -15,7 +15,7 @@ class LoginForm(FlaskForm):
 
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap4(app)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 
